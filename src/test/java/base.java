5f78c4d07c9917f60d64b860c7816710a1fbd3d9
Meldanur Çelik <melda_celik@hotmail.com>
2022-03-06 23:14:49 +0300

    apk and capabilities added

---
+++ src/test/java/base.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class base {
+
+	public static AndroidDriver<AndroidElement> capabilities() throws MalformedURLException {
+
+		File appDir = new File("src");
+		File app = new File(appDir, "ApiDemos-debug.apk");
+
+		DesiredCapabilities cap = new DesiredCapabilities();
+
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 2 XL API 30");
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.0");
+		cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+		cap.setCapability(MobileCapabilityType.APPLICATION_NAME, "uiautomator2");
+
+		URL url = new URL("http://127.0.0.1:4723/wd/hub");
+
+		AndroidDriver<AndroidElement> driver = new AndroidDriver<>(url, cap);
+
+		return driver;
+	}
+
+}
---
+++ src/test/java/basics.java
@@ -0,0 +1,15 @@
+import java.net.MalformedURLException;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class basics extends base {
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+
+		AndroidDriver<AndroidElement> driver = capabilities();
+
+	}
+
+}
