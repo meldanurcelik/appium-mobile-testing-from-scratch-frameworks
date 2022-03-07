832c1ce22aec7cff8a31dfa24055ced0dea14709
LAPTOP-B43UF5O6\melda <meldanur.celik@ginifinance.com>
2022-03-07 16:35:49 +0300

    bug fixed

--- src/test/java/basics.java
+++ src/test/java/basics.java
@@ -1,4 +1,5 @@
 import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
@@ -9,6 +10,9 @@ public class basics extends base {
 		// TODO Auto-generated method stub
 
 		AndroidDriver<AndroidElement> driver = capabilities();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.findElementByXPath("//android.widget.TextView[@content-desc=\"Preference\"]").click();
 
 	}
 
