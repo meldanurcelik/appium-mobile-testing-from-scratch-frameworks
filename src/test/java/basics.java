6dab80e7d1e182ec48f9ff7dab9f7ab51afe58fb
LAPTOP-B43UF5O6\melda <meldanur.celik@ginifinance.com>
2022-03-07 17:40:32 +0300

    findElement id, class, xpath added

--- src/test/java/basics.java
+++ src/test/java/basics.java
@@ -7,12 +7,17 @@ import io.appium.java_client.android.AndroidElement;
 public class basics extends base {
 
 	public static void main(String[] args) throws MalformedURLException {
-		// TODO Auto-generated method stub
 
 		AndroidDriver<AndroidElement> driver = capabilities();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 		driver.findElementByXPath("//android.widget.TextView[@content-desc=\"Preference\"]").click();
+		driver.findElementByXPath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]").click();
+		driver.findElementById("android:id/checkbox").click();
+
+		driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
+
+		System.out.println();
 
 	}
 
