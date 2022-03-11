121947126cb6873e988e3b565513b89acde81dd1
Meldanur Çelik <melda_celik@hotmail.com>
2022-03-11 22:14:24 +0300

    emulator and real device options added

--- src/test/java/basics.java
+++ src/test/java/basics.java
@@ -4,11 +4,11 @@ import java.util.concurrent.TimeUnit;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
-public class basics extends base {
+public class basics extends hybridbase {
 
 	public static void main(String[] args) throws MalformedURLException {
 
-		AndroidDriver<AndroidElement> driver = capabilities();
+		AndroidDriver<AndroidElement> driver = capabilities("real");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 		driver.findElementByXPath("//android.widget.TextView[@content-desc=\"Preference\"]").click();
---
+++ src/test/java/hybridbase.java
@@ -0,0 +1,45 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class hybridbase {
+
+    public static AndroidDriver<AndroidElement> capabilities(String device) throws MalformedURLException {
+
+        File appDir = new File("src");
+        File app = new File(appDir, "ApiDemos-debug.apk");
+
+        DesiredCapabilities cap = new DesiredCapabilities();
+
+        if (device.equals("emulator")) {
+            //emulator
+            cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+            cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.0");
+            cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2_XL_API_30");
+            cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+            cap.setCapability(MobileCapabilityType.APPLICATION_NAME, "uiautomator2");
+
+        } else if (device.equals("real")) {
+            //real device
+            cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+            cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.0");
+            cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Redmi Note 8");
+            cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+            cap.setCapability(MobileCapabilityType.APPLICATION_NAME, "uiautomator2");
+        }
+
+        cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+
+        URL url = new URL("http://127.0.0.1:4723/wd/hub");
+
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<>(url, cap);
+
+        return driver;
+    }
+
+}
