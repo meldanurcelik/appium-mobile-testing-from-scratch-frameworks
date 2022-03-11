5d2d6ef096957e1d60697889ac5986d3d9cafa57
Meldanur Çelik <melda_celik@hotmail.com>
2022-03-11 20:51:01 +0300

    scroll added

---
+++ src/test/java/scrolling.java
@@ -0,0 +1,17 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+public class scrolling extends base {
+    public static void main(String[] args) throws MalformedURLException {
+
+        AndroidDriver<AndroidElement> driver = capabilities();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
+        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"));");
+
+    }
+}
