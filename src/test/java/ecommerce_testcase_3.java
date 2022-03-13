9ea82f4a1385e9a237afcbe9b841245e36738509
Meldanur Çelik <melda_celik@hotmail.com>
2022-03-13 22:08:06 +0300

    Dynamically selecting Product by scanning list based on text added

---
+++ src/test/java/ecommerce_testcase_3.java
@@ -0,0 +1,45 @@
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+public class ecommerce_testcase_3 extends hybridbase{
+
+    public static void main(String[] args) throws MalformedURLException {
+
+        AndroidDriver<AndroidElement> driver = capabilities("emulator");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Hello");
+        driver.hideKeyboard();
+        driver.findElement(By.xpath("//*[@text='Female']")).click();
+        driver.findElement(By.id("android:id/text1")).click();
+        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));");
+        driver.findElement(By.xpath("//*[@text='Argentina']")).click();
+        driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+        driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().resourceId(\"com.androidsample.generalstore:id/rvProductList\")).scrollIntoView(new UiSelector().textMatches(\"Jordan 6 Rings\").instance(0))"));
+
+        int count = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).size();
+
+        for (int i = 0; i < count; i++) {
+
+            String text = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).get(i).getText();
+
+            if (text.equalsIgnoreCase("Jordan 6 Rings")) {
+                driver.findElements(By.id("com.androidsample.generalstore:id/productAddCart")).get(i).click();
+                break;
+            }
+        }
+
+        driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+
+        String lastpageText = driver.findElement(By.id("com.androidsample.generalstore:id/productName")).getText();
+        System.out.println(lastpageText);
+        Assert.assertEquals("Jordan 6 Rings", lastpageText);
+
+    }
+}
