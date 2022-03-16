2afb9d9354eb359847c7555f2346ef4f201c2b1a
LAPTOP-B43UF5O6\melda <meldanur.celik@ginifinance.com>
2022-03-16 16:21:13 +0300

    Prioritizing the Testcases with TestNG

---
+++ src/test/java/TestNGTutorial/logincar.java
@@ -0,0 +1,24 @@
+package TestNGTutorial;
+
+import org.testng.annotations.Test;
+
+public class logincar {
+
+    @Test
+    public void WebLoginCarLoan() {
+        //selenim
+        System.out.println("WebLoginCarLoan");
+    }
+
+    @Test
+    public void MobileLoginCarLoan() {
+        //appium
+        System.out.println("MobileLoginCarLoan");
+    }
+
+    @Test
+    public void LoginAPICarLoan() {
+        //rest API automation
+        System.out.println("LoginAPICarLoan");
+    }
+}
---
+++ src/test/java/TestNGTutorial/loginhome.java
@@ -0,0 +1,25 @@
+package TestNGTutorial;
+
+import org.testng.annotations.Test;
+
+public class loginhome {
+
+    @Test
+    public void WebLoginHomeLoan() {
+        //selenim
+        System.out.println("WebLoginHomeLoan");
+    }
+
+    @Test
+    public void MobileLoginHomeLoan() {
+        //appium
+        System.out.println("MobileLoginHomeLoan");
+    }
+
+    @Test
+    public void LoginAPIHomeLoan() {
+        //rest API automation
+        System.out.println("LoginAPIHomeLoan");
+    }
+
+}
