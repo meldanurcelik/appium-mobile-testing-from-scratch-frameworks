881ffd4abf8ee899ac5cc8597b8ada2fdff38bc3
Meldanur Çelik <melda_celik@hotmail.com>
2022-03-19 23:08:14 +0300

    TestNG Annotations added

--- src/test/java/TestNGTutorial/LoginHome.java
+++ src/test/java/TestNGTutorial/LoginHome.java
@@ -1,6 +1,6 @@
 package TestNGTutorial;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class LoginHome {
 
@@ -20,6 +20,39 @@ public class LoginHome {
     public void LoginAPIHomeLoan() {
         //rest API automation
         System.out.println("LoginAPIHomeLoan");
+    }
+
+
+    @BeforeTest
+    public void Prerequisite() {
+        System.out.println("I will execute first");
+    }
+
+    @AfterTest
+    public void LastExecution() {
+        System.out.println("I will execute last");
+    }
+
+    @BeforeSuite
+    public void Bsuite() {
+        //appium
+        System.out.println("I am no 1");
+    }
+
+    @AfterSuite
+    public void Asuite() {
+        //appium
+        System.out.println("I am no 1 from last");
+    }
+
+    @BeforeMethod
+    public void BeforeEvery() {
+        System.out.println("I will execute before every test method in LoginHome class");
+    }
+
+    @AfterMethod
+    public void AfterEvery() {
+        System.out.println("I will execute after every test method in LoginHome class");
     }
 
 }
